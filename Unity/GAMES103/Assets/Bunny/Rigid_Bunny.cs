302279d28c12acb1eea74a9071d6a31bc8babc62
cas2void <cas2void@gmail.com>
2021-11-28 23:33:10 +0800

    Implement rigid body contacts

--- Unity/GAMES103/Assets/Bunny/Rigid_Bunny.cs
+++ Unity/GAMES103/Assets/Bunny/Rigid_Bunny.cs
@@ -17,7 +17,7 @@ public class Rigid_Bunny : MonoBehaviour
 
 
 	// Use this for initialization
-	void Start () 
+	void Start() 
 	{		
 		Mesh mesh = GetComponent<MeshFilter>().mesh;
 		Vector3[] vertices = mesh.vertices;
@@ -65,10 +65,92 @@ public class Rigid_Bunny : MonoBehaviour
 	//a plane <P, N>
 	void Collision_Impulse(Vector3 P, Vector3 N)
 	{
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		Matrix4x4 rotationMatrix = Matrix4x4.Rotate(transform.rotation);
+
+		int numCollidingVertices = 0;
+		Vector3 sumCollidingPosition = Vector3.zero;
+		Vector3 sumCollidingVelocity = Vector3.zero;
+		foreach (Vector3 position in vertices)
+		{
+			Vector3 rotatedDirection = rotationMatrix.MultiplyPoint3x4(position);
+			Vector3 vertexPosition = transform.position + rotatedDirection;
+			if (Vector3.Dot(vertexPosition - P, N) < 0)
+			{
+				Vector3 vertexVelocity = v + Vector3.Cross(w, rotatedDirection);
+				if (Vector3.Dot(vertexVelocity, N) < 0)
+				{
+					sumCollidingPosition += vertexPosition;
+					sumCollidingVelocity += vertexVelocity;
+					numCollidingVertices++;
+				}
+			}
+		}
+
+		if (numCollidingVertices > 0)
+		{
+			Vector3 Rri = sumCollidingPosition / numCollidingVertices - transform.position;
+			Vector3 vi = sumCollidingVelocity / numCollidingVertices;
+
+			Vector3 vni = Vector3.Dot(vi, N) * N;
+			Vector3 vti = vi - vni;
+
+			float mut = 0.5f;
+			float mun = 0.5f;
+			float a = Mathf.Max(1.0f - mut * (1.0f + mun) * vni.magnitude / vti.magnitude, 0.0f);
+
+			Vector3 vniNew = -mun * vni;
+			Vector3 vtiNew = a * vti;
+			Vector3 viNew = vniNew + vtiNew;
+
+			float inversedMass = 1.0f / mass;
+			Matrix4x4 K = new Matrix4x4(new Vector4(inversedMass, 0, 0, 0), new Vector4(0, inversedMass, 0, 0), new Vector4(0, 0, inversedMass, 0), new Vector4(0, 0, 0, inversedMass));
+			Matrix4x4 crossMatrix = Get_Cross_Matrix(Rri);
+			Matrix4x4 Temp = crossMatrix * I_ref.inverse * crossMatrix;
+			K.SetColumn(0, K.GetColumn(0) - Temp.GetColumn(0));
+			K.SetColumn(1, K.GetColumn(1) - Temp.GetColumn(1));
+			K.SetColumn(2, K.GetColumn(2) - Temp.GetColumn(2));
+			K.SetColumn(3, K.GetColumn(3) - Temp.GetColumn(3));
+
+			Vector3 j = K.inverse.MultiplyPoint3x4(viNew - vi);
+
+			v = v + j * inversedMass;
+			w = w + I_ref.inverse.MultiplyPoint3x4(Vector3.Cross(Rri, j));
+		}
+	}
+
+	void Update_Linear_Velocity()
+	{
+		Vector3 gravity = new Vector3(0.0f, -9.8f, 0.0f);
+		v += gravity * dt;
+		v *= linear_decay;
+	}
+
+	void Update_Angular_Velocity()
+	{
+		w *= angular_decay;
+	}
+
+	void Update_Linear_Status(ref Vector3 position)
+	{
+		position += v * dt;
+	}
+
+	void Update_Angular_Status(ref Quaternion rotation)
+	{
+		Vector3 wt = w * 0.5f * dt;
+		Quaternion deltaq = new Quaternion(wt.x, wt.y, wt.z, 0);
+		deltaq *= rotation;
+		rotation.x += deltaq.x;
+		rotation.y += deltaq.y;
+		rotation.z += deltaq.z;
+		rotation.w += deltaq.w;
+		rotation.Normalize();
 	}
 
 	// Update is called once per frame
-	void Update () 
+	void Update() 
 	{
 		//Game Control
 		if(Input.GetKey("r"))
@@ -84,7 +166,11 @@ public class Rigid_Bunny : MonoBehaviour
 		}
 
 		// Part I: Update velocities
-
+		if (launched)
+		{
+			Update_Linear_Velocity();
+		}
+		Update_Angular_Velocity();
 
 		// Part II: Collision Impulse
 		Collision_Impulse(new Vector3(0, 0.01f, 0), new Vector3(0, 1, 0));
@@ -95,6 +181,12 @@ public class Rigid_Bunny : MonoBehaviour
 		Vector3 x    = transform.position;
 		//Update angular status
 		Quaternion q = transform.rotation;
+
+		if (launched)
+		{
+			Update_Linear_Status(ref x);
+		}
+		Update_Angular_Status(ref q);
 
 		// Part IV: Assign to the object
 		transform.position = x;
