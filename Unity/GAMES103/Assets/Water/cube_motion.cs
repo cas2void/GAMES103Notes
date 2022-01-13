a6b293b67d0cfee49618a1391ba9a83140fb00fa
cas2void <cas2void@gmail.com>
2022-01-13 14:05:59 +0800

    Cleanup

--- Unity/GAMES103/Assets/Water/cube_motion.cs
+++ Unity/GAMES103/Assets/Water/cube_motion.cs
@@ -4,49 +4,55 @@ using UnityEngine;
 
 public class cube_motion : MonoBehaviour
 {
-	bool pressed  =false;
-	public bool cube_move=false;
-	Vector3 offset;
+    bool pressed = false;
+    public bool cube_move = false;
+    Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-	void Update () 
-	{
-		if (Input.GetMouseButtonDown (0)) 
-		{
-			pressed = true;
-			Ray ray=Camera.main.ScreenPointToRay (Input.mousePosition);
-			if(Vector3.Cross(ray.direction, transform.position - ray.origin).magnitude<0.8f)	cube_move=true;
-			else 																				cube_move=false;
-			offset = Input.mousePosition - Camera.main.WorldToScreenPoint (transform.position);
-		}
-		if (Input.GetMouseButtonUp (0))
-		{
-			pressed = false;
-			cube_move = false;
-		}
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            pressed = true;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Vector3.Cross(ray.direction, transform.position - ray.origin).magnitude < 0.8f)
+            {
+                cube_move = true;
+            }
+            else
+            {
+                cube_move = false;
+            }
+            offset = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            pressed = false;
+            cube_move = false;
+        }
 
-		if(pressed)
-		{
-			if(cube_move)
-			{
-				Vector3 mouse=Input.mousePosition;
-				mouse -= offset;
-				mouse.z = Camera.main.WorldToScreenPoint (transform.position).z;
-				Vector3 p= Camera.main.ScreenToWorldPoint (mouse);
-				p.y=transform.position.y;
-				transform.position =p;
-			}
-			else
-			{
-				//float h = 2.0f * Input.GetAxis("Mouse X");
-				//Camera.main.transform.RotateAround(new Vector3(0, 0, 0), Vector3.up, h);
-			}
-		}
-	}
+        if (pressed)
+        {
+            if (cube_move)
+            {
+                Vector3 mouse = Input.mousePosition;
+                mouse -= offset;
+                mouse.z = Camera.main.WorldToScreenPoint(transform.position).z;
+                Vector3 p = Camera.main.ScreenToWorldPoint(mouse);
+                p.y = transform.position.y;
+                transform.position = p;
+            }
+            else
+            {
+                //float h = 2.0f * Input.GetAxis("Mouse X");
+                //Camera.main.transform.RotateAround(new Vector3(0, 0, 0), Vector3.up, h);
+            }
+        }
+    }
 }
